13e4cc79604b1bc5fdc258f54bcff5b27d96bf7f
Dakota <dakota.reed.28@gmail.com>
2022-10-07 16:23:11 +0300

    Initial commit

---
+++ src/test/java/Lesson01_TestNG/TestNG_Example.java
@@ -0,0 +1,41 @@
+package Lesson01_TestNG;
+
+import org.testng.annotations.*;
+
+public class TestNG_Example {
+
+    @BeforeClass
+    public void startSession() {
+        System.out.println("Before Class");
+    }
+    @AfterClass
+    public void closeSession() {
+        System.out.println("After Class");
+    }
+    @BeforeMethod
+    public void beforeMethod() {
+        System.out.println("Before Method");
+    }
+    @AfterMethod
+    public void afterMethod() {
+        System.out.println("After Method");
+    }
+
+    @Test
+    public void test01() {
+        System.out.println("test01");
+    }
+    @Test
+    public void test02() {
+        System.out.println("test02");
+    }
+    @Test (priority = 0, enabled = false)
+    public void test04() {
+        System.out.println("test04");
+    }
+    @Test (dependsOnMethods = {"test01", "test02"})
+    public void test03() {
+        System.out.println("test03");
+    }
+
+}
