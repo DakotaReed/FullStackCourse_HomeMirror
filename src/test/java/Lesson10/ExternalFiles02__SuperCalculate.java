cf36a8094551c00eb97cad2bd7bca93f53b8b859
Dakota <dakota.reed.28@gmail.com>
2022-10-10 15:47:15 +0300

    Comment

--- src/test/java/Lesson10/ExternalFiles02__SuperCalculate.java
+++ src/test/java/Lesson10/ExternalFiles02__SuperCalculate.java
@@ -14,7 +14,7 @@ import org.testng.annotations.Test;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
-public class ExternalFiles02 {
+public class ExternalFiles02__SuperCalculate {
 
     WebDriver driver;
     @BeforeClass
