43ac11adbe3b4a6279f7f03e996795ea7bfeeed3
Dakota <dakota.reed.28@gmail.com>
2022-10-11 14:43:23 +0300

    Comment

---
+++ src/test/java/Pattern__AutomationListeners.java
@@ -0,0 +1,36 @@
+import org.testng.ITestContext;
+import org.testng.ITestListener;
+import org.testng.ITestResult;
+import org.testng.annotations.Listeners;
+
+//@Listeners(Pattern__AutomationListeners.class)
+
+public class Pattern__AutomationListeners implements ITestListener {
+    public void onStart(ITestContext execution){
+        // TODO Auto-generated method stub
+    }
+
+    public void onFinish(ITestContext execution){
+        // TODO Auto-generated method stub
+    }
+
+    public void onTestStart(ITestResult test){
+        // TODO Auto-generated method stub
+    }
+
+    public void onTestSuccess(ITestResult test){
+        // TODO Auto-generated method stub
+    }
+
+    public void onTestFailure(ITestResult test){
+        // TODO Auto-generated method stub
+    }
+
+    public void onTestFailedButWithinSuccessPercentage(ITestResult test) {
+        // TODO Auto-generated method stub
+    }
+
+    public void onTestSkipped(ITestResult test){
+        // TODO Auto-generated method stub
+    }
+}
---
+++ src/test/java/Pattern__GetData.java
@@ -0,0 +1,48 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+import org.w3c.dom.Document;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import java.io.File;
+
+public class Pattern__GetData {
+
+    WebDriver driver;
+    @BeforeClass
+    public void startSession() {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get(getData("Url"));
+    }
+    @AfterClass
+    public void closeSession() throws InterruptedException {
+        Thread.sleep(3000);
+        driver.quit();
+    }
+
+    @Test
+    public void Test01() {
+
+    }
+
+    public String getData (String nodeName) {
+        DocumentBuilder dBuilder;
+        Document doc = null;
+        File fXmlFile = new File("./Configuration.xml");
+        DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+        try {
+            dBuilder = dbFactory.newDocumentBuilder();
+            doc = dBuilder.parse(fXmlFile);        }
+        catch(Exception e) {
+            System.out.println("Exception in reading XML file: " + e);        }
+        doc.getDocumentElement().normalize();
+        return doc.getElementsByTagName(nodeName).item(0).getTextContent();
+    }
+
+}
