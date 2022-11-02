f75a84edc658f4b5e8b95839b10705cd5bf29fc0
Dakota <dakota.reed.28@gmail.com>
2022-11-02 18:34:03 +0200

    Comment

--- SeleniumV2/src/test/java/Lesson08__Controllers_and_AssertsVerifications/Loudev__Support.java
+++ SeleniumV2/src/test/java/Lesson08__Controllers_and_AssertsVerifications/Loudev__Support.java
@@ -10,8 +10,8 @@ import static org.testng.AssertJUnit.assertTrue;
 public class Loudev__Support {
     public static String numberOfElem;
     public static void verifyElements(WebDriver driverOut) {
-        WebElement leftTable = driverOut.findElement(By.className("ms-container"));
-        List<WebElement> list01ofElements = leftTable.findElements(By.className("ms-elem-selectable"));
+        WebElement table = driverOut.findElement(By.className("ms-container"));
+        List<WebElement> list01ofElements = table.findElements(By.className("ms-elem-selectable"));
         for (int i=0; i< list01ofElements.size(); i++) {
             if (list01ofElements.get(i).isDisplayed()) {
                 numberOfElem = Integer.toString(i+1);
